d0240d91ef34fc304b2748d987713984bf87f666
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-10-11 09:39:45 +0100

    create eslint-prettier-typscript template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        hello marusoft
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit incidunt itaque odit maxime repudiandae minima provident? Sapiente fuga
+          fugiat ab facilis ullam recusandae vitae totam nihil blanditiis, eius odit consequatur?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>kdkdkk</p>
+        <input type="text" placeholder="" />
+        <p></p>
       </header>
+      <p></p>
     </div>
   );
-}
+};
 
 export default App;
