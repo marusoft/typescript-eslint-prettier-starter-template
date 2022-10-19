9bf2461e4a6ccd7d8ab8657a0255effe2e200e54
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-10-19 10:22:55 +0100

    create starter template

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,8 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        hello marusoft
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit incidunt itaque odit maxime repudiandae minima provident? Sapiente fuga
-          fugiat ab facilis ullam recusandae vitae totam nihil blanditiis, eius odit consequatur?
-        </p>
-        <p>kdkdkk</p>
-        <input type="text" placeholder="" />
-        <p></p>
-      </header>
-      <p></p>
+      <header className="App-header">helloooooooo</header>
+      <p>git add</p>
     </div>
   );
 };
